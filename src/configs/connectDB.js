17886a4b2c7898dcbb7a876c074b31024f2aec7e
Vũ Minh Quân <vuminhquan2904@gmail.com>
2022-11-22 01:43:44 +0700

    second

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -6,9 +6,9 @@ console.log("Creating connection pool...");
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    database: 'nodejsbasic',
-    // password: 'password'
+    user: 'phxwxfzd_crud-db',
+    database: 'phxwxfzd_crud-db',
+    password: '1234'
 })
 
 
